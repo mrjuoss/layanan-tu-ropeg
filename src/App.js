93ba59124201e04e676e05a64ae732bc26996f7d
Mohamad Arif Mujaki <mrjuoss@gmail.com>
2020-10-08 23:15:38 +0700

    Add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import "./css/dist/tailwind.css";
 
 function App() {
  return (
-   <div>
-    <p>Hello Word</p>
-   </div>
+< div className = "flex max-w-md p-6 mx-auto mt-10 bg-gray-100 rounded-lg shadow-xl" >
+  <
+  div className = "pt-1 ml-6" >
+  <
+  h1 className = "text-2xl leading-tight text-blue-700" >
+  Tailwind and Create React App <
+  /h1> <
+  p className = "text-base leading-normal text-gray-700" >
+  Building apps together with @mrjuoss<
+  /p> <
+  /div> <
+  /div>
  )
 }
 
