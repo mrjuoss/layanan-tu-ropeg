94841bd6b17a816f3910ca553557c37ae4be32f7
Mohamad Arif Mujaki <mrjuoss@gmail.com>
2020-10-11 23:21:36 +0700

    Test Small Device

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,13 +8,13 @@ function Navigation({logoImage}) {
     <div className = "container mx-auto flex justify-between items-center">
       <img src={`img/${logoImage}`} alt="" />
       <ul className="flex flex-row">
-         <li className="text-white text-sm">
+         <li className="text-white text-sm sm:text:xs">
            <a href="#home" className="bg-purple-900 mr-2 px-8 py-2 rounded hover:bg-purple-200 hover:font-semibold hover:text-purple-900">Home</a>
          </li>
-         <li className="text-white text-sm">
+         <li className="text-white text-sm visible sm:invisible">
            <a href="#layanan" className="bg-purple-900 mx-2 px-8 py-2 rounded hover:bg-purple-200 hover:font-semibold hover:text-purple-900">Layanan</a>
          </li>
-         <li className="text-white text-sm">
+         <li className="text-white text-sm visible sm:invisible">
            <a href="#kontak" className="bg-purple-900 mx-2 px-8 py-2 rounded hover:bg-purple-200 hover:font-semibold hover:text-purple-900">Kontak</a>
          </li>
       </ul>
