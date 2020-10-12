8bb5e9d1bbb1370001eb73e00dc6d03288eda142
Mohamad Arif Mujaki <mrjuoss@gmail.com>
2020-10-12 11:03:44 +0700

    Layanan Responsive

--- src/components/Layanan.js
+++ src/components/Layanan.js
@@ -4,11 +4,11 @@ import "../css/dist/tailwind.css";
 
 function Layanan({jenisLayanan, cardImage}) {
   return (
-    <div className="w-64 mb-4 mb:mx-20">
+    <div className="w-64 pl-4 mx-auto mb-4 mb:mx-20">
       <a href="https://google.com"
       className="group hover:transform hover:scale-75">
         <img src={`img/${cardImage}`} alt="" className="w-56" />
-        <p className="w-56 mb-4 bg-purple-800 text-center text-white py-2 shadow-lg"> {jenisLayanan} </p>
+        <p className="w-56 py-2 mb-4 text-center text-white bg-purple-800 shadow-lg"> {jenisLayanan} </p>
       </a>
     </div>
   )
